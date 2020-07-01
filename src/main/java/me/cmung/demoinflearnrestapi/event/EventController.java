d0f535864710302784aa0b4d687b8a77e7058ca1
MUNKYUNG CHOI <chl30shl20@gmail.com>
2020-07-02 00:41:11 +0900

    DTO validation

--- src/main/java/me/cmung/demoinflearnrestapi/event/EventController.java
+++ src/main/java/me/cmung/demoinflearnrestapi/event/EventController.java
@@ -1,6 +1,7 @@
 package me.cmung.demoinflearnrestapi.event;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.Errors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo;
@@ -28,9 +30,12 @@ public class EventController {
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
-        Event event = modelMapper.map(eventDto, Event.class);
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+        if(errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
 
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
         newEvent.setId(10);
