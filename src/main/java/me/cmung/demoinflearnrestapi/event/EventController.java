3319f5ed0144df4b5ecd4b354bfe7475d3c53699
munkyung <chl30shl20@gmail.com>
2020-07-04 20:22:08 +0900

    HATEOAS

--- src/main/java/me/cmung/demoinflearnrestapi/event/EventController.java
+++ src/main/java/me/cmung/demoinflearnrestapi/event/EventController.java
@@ -3,6 +3,7 @@ package me.cmung.demoinflearnrestapi.event;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -46,9 +47,14 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update(); // domain 객체에서 Service 로직 처리중
         Event newEvent = eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
         newEvent.setId(10);
-        return ResponseEntity.created(createdUri).body(newEvent);
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withSelfRel());
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+        return ResponseEntity.created(createdUri).body(eventResource);
     }
 
 }
